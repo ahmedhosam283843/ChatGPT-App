d33ad41771aac99fa8e79605f969078a05759e44
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-03-08 09:11:45 +0200

    feat: show models on screen using future builder

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -36,27 +36,7 @@ final chatMessages = [
   },
 ];
 
-List<String> models = [
-  'Model1',
-  'Model2',
-  'Model3',
-  'Model4',
-  'Model5',
-  'Model6',
-];
 
-List<DropdownMenuItem<String>>? get getModelsItem {
-  List<DropdownMenuItem<String>>? modelsItems =
-      List<DropdownMenuItem<String>>.generate(
-          models.length,
-          (index) => DropdownMenuItem(
-              value: models[index],
-              child: TextWidget(
-                label: models[index],
-                fontSize: 15,
-              )));
-  return modelsItems;
-}
 
 String BASE_URL = "https://api.openai.com/v1";
 String API_KEY = YOUR_API_KEY;
--- lib/widgets/drop_down_widget.dart
+++ lib/widgets/drop_down_widget.dart
@@ -1,3 +1,6 @@
+import 'package:chatgpt_app/services/api_sevices.dart';
+import 'package:chatgpt_app/widgets/text_widget.dart';
+
 import '../constants/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +12,39 @@ class ModelsDrowDownWidget extends StatefulWidget {
 }
 
 class _ModelsDrowDownWidgetState extends State<ModelsDrowDownWidget> {
-  String currentModel = "Model1";
+  String currentModel = "text-davinci-003";
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      dropdownColor: scaffoldBackgroundColor,
-      iconEnabledColor: Colors.white,
-      value: currentModel,
-      items: getModelsItem,
-      onChanged: (value) {
-        setState(() {
-          currentModel = value.toString();
-        });
+    return FutureBuilder(
+      future: ApiService.getModels(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: TextWidget(label: snapshot.error.toString()),
+          );
+        }
+        return snapshot.data == null || snapshot.data!.isEmpty
+            ? const SizedBox.shrink()
+            : FittedBox(
+                child: DropdownButton(
+                  dropdownColor: scaffoldBackgroundColor,
+                  iconEnabledColor: Colors.white,
+                  items: List<DropdownMenuItem<String>>.generate(
+                      snapshot.data!.length,
+                      (index) => DropdownMenuItem(
+                          value: snapshot.data![index].id,
+                          child: TextWidget(
+                            label: snapshot.data![index].id,
+                            fontSize: 15,
+                          ))),
+                  value: currentModel,
+                  onChanged: (value) {
+                    setState(() {
+                      currentModel = value.toString();
+                    });
+                  },
+                ),
+              );
       },
     );
   }
